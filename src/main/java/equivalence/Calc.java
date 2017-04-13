1c50a23715373313a20355f0195eb924e38bc8ae
Florian <Florian@DESKTOP-LIFMANE.fritz.box>
2017-04-13 17:35:06 +0200

    Finished Exercise Set 9 Equivalence Classes c0: 2 c1: 3 cInfinite: 4

--- src/main/java/equivalence/Calc.java
+++ src/main/java/equivalence/Calc.java
@@ -1,9 +1,34 @@
 package equivalence;
 
+import java.time.DateTimeException;
+
 public class Calc {
 	
 	public static double dailyWage(Profession prof, int day, double hours) {
-		return 0;
+		
+		double wage = 0.00d;
+		
+		switch (prof) {
+		case BAKER: wage = 10.0d; break;
+		case TEACHER: wage = 20.0d; break;
+		case DOCTOR: wage = 30.0d; break;
+		case RETIRED: break;
+		case UNEMPLOYED: break;
+		}
+		
+		if(hours > 12 || hours < 0) {
+			throw new DateTimeException("Too many hours");
+		} else {
+			wage *= hours;
+		}
+		
+		if(day > 5 && day < 8){
+			wage *= 1.1d;
+		} else if (day < 1 || day > 7) {
+			throw new DateTimeException("weekdays out of bound");
+		}
+		
+		return wage;
 	}
 
 	public static int monthlyCharge(int duration) {
--- src/test/java/equivalence/CalcTest.java
+++ src/test/java/equivalence/CalcTest.java
@@ -2,6 +2,8 @@ package equivalence;
 
 import static org.junit.Assert.*;
 
+import java.time.DateTimeException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,42 +18,70 @@ public class CalcTest {
 	public void tearDown() throws Exception {
 	}
 
-	@Test
+	@Test(expected=NullPointerException.class)
 	public void invalidProfession() {
 		assertEquals(0, Calc.dailyWage(null, 0, 0),0.001d);
 	}
 	
 	@Test
-	public void validProfessionBaker() {
-		assertEquals(0, Calc.dailyWage(Profession.BAKER, 0, 0),0.001d);
+	public void validProfessionBakerValidDayValidHours() {
+		assertEquals(120, Calc.dailyWage(Profession.BAKER, 1, 12),0.001d);
 	}
 
 	@Test
-	public void validProfessionDoctor() {
-		assertEquals(0, Calc.dailyWage(Profession.DOCTOR, 0, 0),0.001d);
+	public void validProfessionDoctorValidBonusDayValidHours() {
+		assertEquals(396, Calc.dailyWage(Profession.DOCTOR, 6, 12),0.001d);
 	}
 	
 	@Test
-	public void validProfessionRetired() {
-		assertEquals(0, Calc.dailyWage(Profession.RETIRED, 0, 0),0.001d);
+	public void validProfessionTeacherValidDayValidHours() {
+		assertEquals(200, Calc.dailyWage(Profession.TEACHER, 1, 10),0.001d);
 	}
 	
-	@Test
-	public void validProfessionTeacher() {
-		assertEquals(0, Calc.dailyWage(Profession.TEACHER, 0, 0),0.001d);
+	@Test(expected=DateTimeException.class)
+	public void validProfessionRetiredInvalidDayLower() {
+		assertEquals(0, Calc.dailyWage(Profession.RETIRED, 0, 10),0.001d);
 	}
 	
-	@Test
-	public void validProfessionUnemployed() {
-		assertEquals(0, Calc.dailyWage(Profession.UNEMPLOYED, 0, 0),0.001d);
+	@Test(expected=DateTimeException.class)
+	public void validProfessionUnemployedInvalidDayUpper() {
+		assertEquals(0, Calc.dailyWage(Profession.UNEMPLOYED, 8, 10),0.001d);
 	}
 	
+	@Test(expected=DateTimeException.class)
+	public void validProfessionUnemployedInvalidHoursLower() {
+		assertEquals(0, Calc.dailyWage(Profession.UNEMPLOYED, 1, -1),0.001d);
+	}
 	
-	
-	
+	@Test(expected=DateTimeException.class)
+	public void validProfessionUnemployedInvalidHoursUpper() {
+		assertEquals(0, Calc.dailyWage(Profession.UNEMPLOYED, 1, 13),0.001d);
+	}
 	
 	/*
 	 * TEST OF MONTHLY CHARGE
 	 */
-
+	
+	@Test
+	public void statementCoverageDurationLower() {
+		assertEquals(2500, Calc.monthlyCharge(200));
+	}
+	
+	@Test
+	public void statementCoverageDurationUpper() {
+		assertEquals(2503, Calc.monthlyCharge(201));
+	}
+	
+	@Test
+	public void branchCoverageDurationLower() {
+		assertEquals(0, Calc.monthlyCharge(-1));
+	}
+	
+	@Test
+	public void pathCoverageDurationLower() {
+		assertEquals(2000, Calc.monthlyCharge(0));
+	}
+	
+	
+	
 }
